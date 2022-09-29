7affcb3ac97d4536b044c62a98e0250d368d1340
ArjunNarayanan <arjun.narayanan91@gmail.com>
2022-09-29 08:32:13 -0700

    no major changes

--- test/test_plot.jl
+++ test/test_plot.jl
@@ -1,5 +1,4 @@
 using PyPlot
-pygui(false)
 
 using Random
 using Revise
@@ -13,13 +12,17 @@ QM = QuadMeshGame
 
 elem_color = [0.8, 1, 0.8]
 
-Random.seed!(123)
-mesh = RQ.random_quad_mesh(10)
-mesh = QM.QuadMesh(mesh.p, mesh.t, mesh.t2t, mesh.t2n)
 
-QM.left_flip!(mesh, 16, 2)
-QM.right_flip!(mesh, 2, 3)
-QM.split!(mesh, 13, 3)
+mesh = QM.square_mesh(2)
 QM.split!(mesh, 3, 3)
-fig, ax = PQ.plot_mesh(QM.active_vertex_coordinates(mesh), QM.active_quad_connectivity(mesh), elem_numbers=true, node_numbers=true)
+fig, ax = PQ.plot_mesh(
+    QM.active_vertex_coordinates(mesh),
+    QM.active_quad_connectivity(mesh);
+    elem_numbers = true,
+    node_numbers = true,
+)
 fig
+
+# Random.seed!(123)
+# mesh = RQ.random_quad_mesh(10)
+# mesh = QM.QuadMesh(mesh.p, mesh.t, mesh.t2t, mesh.t2n)
