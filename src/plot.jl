8d020a589e739fef392f5dc1421b0fa68516eb65
ArjunNarayanan <arjun.narayanan91@gmail.com>
2022-10-06 17:22:32 -0700

    added method to plot valences

--- src/plot.jl
+++ src/plot.jl
@@ -10,6 +10,7 @@ function plot_mesh!(
     elem_numbers = false,
     node_numbers = false,
     elem_color = [0.8, 1.0, 0.8],
+    vertex_score = []
 )
     ts = Array([connectivity[1:3, :] connectivity[[1, 3, 4], :]]' .- 1)
 
@@ -23,15 +24,15 @@ function plot_mesh!(
     xy = vcat([[points[:, [el; el[1]]]'; NaN NaN] for el in eachcol(connectivity)]...)
     ax.plot(xy[:, 1], xy[:, 2], "k", linewidth = 1.0)
 
-    tpars = Dict(
-        :color => "k",
-        :horizontalalignment => "center",
-        :verticalalignment => "center",
-        :fontfamily => "sans-serif",
-        :fontsize => 10,
-    )
-
     if elem_numbers
+
+        tpars = Dict(
+            :color => "k",
+            :horizontalalignment => "center",
+            :verticalalignment => "center",
+            :fontfamily => "sans-serif",
+            :fontsize => 10,
+        )
         for (idx, nodes) in enumerate(eachcol(connectivity))
             pc = sum(points[:, nodes], dims = 2) / 4
             ax.text(pc[1], pc[2], "$idx"; tpars...)
@@ -39,6 +40,14 @@ function plot_mesh!(
     end
 
     if node_numbers
+
+        tpars = Dict(
+            :color => "k",
+            :horizontalalignment => "center",
+            :verticalalignment => "center",
+            :fontfamily => "sans-serif",
+            :fontsize => 10,
+        )
         plot(
             points[1, :],
             points[2, :],
@@ -51,6 +60,33 @@ function plot_mesh!(
             ax.text(point[1], point[2], "$idx"; tpars...)
         end
     end
+
+    if length(vertex_score) > 0
+        @assert length(vertex_score) == size(points, 2) "Dimension mismatch between vertex scores and points"
+
+        tpars = Dict(
+            :color => "w",
+            #:fontfamily=>"sans-serif",
+            :horizontalalignment => "center",
+            :verticalalignment => "center",
+            :fontsize => 12,
+            :fontweight => "bold",
+        )
+
+        ax.scatter(points[1, vertex_score.<0], points[2, vertex_score.<0], 450, color = "r")
+        ax.scatter(points[1, vertex_score.>0], points[2, vertex_score.>0], 450, color = "m")
+        # ax.scatter(m.p[dd.==0, 1], m.p[dd.==0, 2], 450, color = "b")
+        for (i, point) in enumerate(eachcol(points))
+            if vertex_score[i] != 0
+                txt = string(vertex_score[i])
+                if vertex_score[i] > 0
+                    txt = "+" * txt
+                end
+                ax.text(point[1], point[2], txt; tpars...)
+            end
+        end
+    end
+
 end
 
 function plot_mesh(
@@ -59,6 +95,7 @@ function plot_mesh(
     elem_numbers = false,
     node_numbers = false,
     elem_color = [0.8, 1.0, 0.8],
+    vertex_score = []
 )
 
     fig, ax = subplots()
@@ -71,6 +108,7 @@ function plot_mesh(
         elem_numbers = elem_numbers,
         node_numbers = node_numbers,
         elem_color = elem_color,
+        vertex_score = vertex_score
     )
     return fig, ax
 end
--- test/test_plot.jl
+++ test/test_plot.jl
@@ -15,11 +15,11 @@ elem_color = [0.8, 1, 0.8]
 
 mesh = QM.square_mesh(2)
 QM.split!(mesh, 3, 3)
+vs = [0,-1,0,1,2,1,0,-1,1,-1]
 fig, ax = PQ.plot_mesh(
     QM.active_vertex_coordinates(mesh),
     QM.active_quad_connectivity(mesh);
-    elem_numbers = true,
-    node_numbers = true,
+    vertex_score = vs
 )
 fig
 
